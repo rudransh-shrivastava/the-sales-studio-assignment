be15ec1feda7ae526a2f3604997b3965d09dfd7b
Rudransh Shrivastava <rudransh.shr@gmail.com>
2025-03-26 00:28:51 +0530

    claim coupon api

--- src/app/api/claim/route.ts
+++ src/app/api/claim/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
 
+const COOLDOWN_PERIOD = 1000 * 60; // 1 minute
 export async function POST(req: Request) {
   const { couponCode } = await req.json();
   if (!couponCode) {
@@ -9,11 +11,50 @@ export async function POST(req: Request) {
     );
   }
 
-  const forwarded = req.headers.get("x-forwarded-for");
-  console.log(forwarded);
+  const ip = req.headers.get("x-forwarded-for");
+  console.log(ip);
+
+  // TODO: do it here
+
+  const coupon = await prisma.coupon.findFirst({
+    where: {
+      code: couponCode,
+    },
+  });
+
+  if (!coupon) {
+    return NextResponse.json({ error: "Invalid coupon code" }, { status: 400 });
+  }
+  if (!coupon.isActive) {
+    return NextResponse.json(
+      {
+        error:
+          "Coupon is not active (generally we would not tell the client this but its ok in this example for testing purposes only)",
+      },
+      { status: 400 },
+    );
+  }
+
+  const cooldownThreshold = new Date(Date.now() - COOLDOWN_PERIOD);
+  const existingIpClaim = await prisma.claimHistory.findFirst({
+    where: {
+      couponId: coupon.id,
+      ipAddress: ip as string,
+      createdAt: { gte: cooldownThreshold },
+    },
+  });
+
+  if (existingIpClaim) {
+    return NextResponse.json(
+      { error: "You have already claimed this coupon, please try again later" },
+      { status: 400 },
+    );
+  }
+
+  // Else we create a new claim history
 
   return NextResponse.json(
-    { message: `Coupon claimed successfully! forwaded = > ${forwarded}` },
+    { message: "Coupon claimed successfully!" },
     { status: 200 },
   );
 }
